e99197f0638586074bf8e2e63421ad7bb7607d79
Ihor Triapitsyn <triapitsyn90@gmail.com>
2022-01-07 22:46:52 +0200

    Create template for notes

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const defaultNotes = [
+    {category: "Idea", content: "Create cool app", archived: false, name: "New idea", id: 1},
+    {category: "Random Thought", content: "Today a good weather", archived: false, name: "Weather", id: 2},
+    {category: "Task", content: "Tomatoes, apples", archived: false, name: "Shopping list", id: 3},
+    {category: "Task", content: "Clean house", archived: false, name: "Do homework", id: 4}
+];
+
+const taskImg = "<i class=\"fas fa-tasks\"></i>";
+const ideaImg = "<i class=\"far fa-lightbulb\"></i>";
+const randomImg = "<i class=\"fas fa-dice\"></i>";
